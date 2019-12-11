c0ad66cc753ffed27037cfeea55a7b15d9a325be
LegusX <loghenrie@gmail.com>
2019-12-10 23:13:16 -0600

    PROGRESS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-window.onload = function() {
-    setup()
-}
+window.onload = function(){setup()}
+
+//object to store all color values
+//values are stored in their rows, each array within "pixels" is a row, which in turn contains the individual arrays for each pixels' color data in standard rgb format
+//NOTE: transparency will be ignored in all color calculations
+var pixels = []
+
+//object to store the completed lists of matched blocks
+var finishedBlocks = []
+
+//object that stores the calculated average colors of all blocks
+var blockList = {}
 
 //Init all html elements and just get the program ready in general
 function setup() {
@@ -54,24 +63,27 @@ function setup() {
         document.getElementById("fileupload").value = null;
         document.getElementById("pixelReader").remove();
     })
+
+    //get map.json and convert it to an object
+    fetch("./texture_average/map.json").then(response => response.json()).then((data) => {
+        blockList = data
+    })
 }
 
 function readImage() {
     let canvas = document.getElementById("pixelReader")
     let ctx = canvas.getContext("2d")
 
-    //object to store all color values
-    //values are stored in their rows, each array within "pixels" is a row, which in turn contains the individual arrays for each pixels' color data in standard rgb format
-    //NOTE: transparency will be ignored in all color calculations
-    var pixels = []
-
     // begin scanning through each and every pixel
     let x = 0;
     let y = 0;
 
     for (let i = 0; i<canvas.width*canvas.height; i++ ) {
-        //check to see if a new array within pixels needs to be created for the new row
-        if (x === 0) pixels[y] = [];
+        //check to see if a new array within pixels/finishedBlocks needs to be created for the new row
+        if (x === 0) {
+            pixels[y] = [];
+            finishedBlocks[y] = []
+        }
         let pixel = ctx.getImageData(x,y,1,1);
         let data = pixel.data;
         
@@ -89,5 +101,109 @@ function readImage() {
             y++
         }
     }
-    console.log(pixels.slice(-1)[0])
+    convertImage()
+}
+
+function convertImage() {
+    //lots and lots of loops that'll probably end up crashing your computer :D
+    let canvas = document.getElementById("pixelReader")
+    for (let y in pixels) {
+        for (let x in pixels[y]) {
+            let limits = [5, 5, 5] //how far off we will initially allow the colors to be
+            let pixel = pixels[y][x]
+            let pixDec = parseInt(pixel[0].toString(16)+pixel[1].toString(16)+pixel[2].toString(16), 16)
+            
+            // cycle through the blocks until we find the closest one
+            // while (true) {
+            //     let potential = [] // list of potential blocks that will gradually be narrowed down
+
+            //     //filter through to check for red matches
+            //     for (let i in Object.getOwnPropertyNames(blockList)) {
+            //         let block = blockList[Object.getOwnPropertyNames(blockList)[i]]
+            //         if (Math.abs(block[0]-pixel[0]) <= limits[0])
+            //          potential.push(Object.getOwnPropertyNames(blockList)[i])
+            //     }
+            //     if (potential.length < 1) {
+            //         limits[0]+=5
+            //         continue;
+            //     }
+
+            //     //filter through to check for green matches
+            //     for (let i in potential) {
+            //         let block = blockList[potential[i]]
+            //         if (!(block[1]+limits[1] >= pixel[1] && block[1]-limits[1] <= pixel[1])) {
+            //             potential.splice(i,1) //remove if it doesn't meet the requirements
+            //         } 
+            //     }
+            //     if (potential.length < 1) {
+            //         if (limits[0] > limits[1]) limits[1]+=5
+            //         else limits[0]+=5
+            //         continue;
+            //     }
+
+            //     //filter through to check for blue matches
+            //     for (let i in potential) {
+            //         let block = blockList[potential[i]]
+            //         if (!(block[2]+limits[2] >= pixel[2] && block[2]-limits[2] <= pixel[2])) {
+            //             potential.splice(i,1) //remove if it doesn't meet the requirements
+            //         } 
+            //     }
+            //     if (potential.length < 1) {
+            //         if (limits[1] > limits[2]) limits[2]+=5
+            //         else limits[1]+=5
+            //         continue;
+            //     }
+
+            //     //by this point, at least one potential block should remain, but still need to check if there is more than one block that meets the limit criteria
+            //     if (potential.length === 1) {
+            //         finishedBlocks[y][x] = potential[0];
+            //         //reset limits for next pixel
+            //         limits = [5,5,5]
+            //         break;
+            //     }
+            //     else {
+            //         //Do some simple subtraction to see which block has the least total difference with the pixel in question.
+            //         let leastDifference = []
+            //         for (let i in potential) {
+            //             let block = blockList[potential[i]]
+
+            //             let redDiff = Math.abs(block[0]-pixel[0])
+            //             let greenDiff = Math.abs(block[1]-pixel[1])
+            //             let blueDiff = Math.abs(block[2]-pixel[2])
+
+            //             leastDifference[i] = redDiff+greenDiff+blueDiff
+            //             leastDifference[i]+= potential[i]
+            //         }
+
+            //         //sort leastDifference to see which one is the least:
+            //         leastDifference.sort(function(a,b){
+            //             //weird jankiness here because I chose to do so
+            //             return Number(a.match(/[0-9]*/g))-Number(b.match(/[0-9]*/g))
+            //         })
+            //         finishedBlocks[y][x] = leastDifference[0].match(/[a-zA-Z]*\.[a-zA-Z]*/g)
+            //         //reset limits for next pixel
+            //         limits = [5,5,5]
+            //         break;
+            //     }
+            // }
+
+            //new looping thing
+            let limit = 1000
+            let potential = ""
+            for (let i in Object.getOwnPropertyNames(blockList)) {
+                let block = blockList[Object.getOwnPropertyNames(blockList)[i]]
+                if (Math.sqrt(Math.pow(pixel[0]-block[0],2) + Math.pow(pixel[1]-block[1],2) + Math.pow(pixel[2]-block[2],2)) < limit) {
+                    finishedBlocks[y][x] = Object.getOwnPropertyNames(blockList)[i]
+                    limit = Math.sqrt(Math.pow(pixel[0]-block[0],2) + Math.pow(pixel[1]-block[1],2) + Math.pow(pixel[2]-block[2],2))
+                }
+            }
+
+            //increment x position, and move to next row as needed
+            x++
+            if (x>canvas.width) {
+                x = 0;
+                y++
+            }
+        }
+    }
 }
--- texture_average/averager.js
+++ texture_average/averager.js
@@ -1,10 +1,33 @@
 var pix = require("image-pixels")
 var fs = require("fs")
 
+var fileInfo = {}
 let files = fs.readdirSync("../mc_textures")
+let i = 0
 for (let i in files) {
-    pix("../mc_textures/"+files[i]).then((data)=>{
-        //get all red pixels and average
-        for (let j = 0; i<data)
+    pix("../mc_textures/"+files[i]).then(({data, width, height})=>{
+
+        let preRed = 0 //total of all red values
+        for (let j = 0; j<data.length; j+=4) {
+            preRed += data[j]
+        }
+        let endRed = Math.floor(preRed/(data.length/4))
+        
+        let preGreen = 0 //total of all green values
+        for (let j = 1; j<data.length; j+=4) {
+            preGreen += data[j]
+        }
+        let endGreen = Math.floor(preGreen/(data.length/4))
+
+        let preBlue = 0 //total of all blue values
+        for (let j = 2; j<data.length; j+=4) {
+            preBlue += data[j]
+        }
+        let endBlue = Math.floor(preBlue/(data.length/4))
+
+        fileInfo[files[i]] = [endRed, endGreen, endBlue, "rgba("+endRed+", "+endGreen+", "+endBlue+")", hex]
+        fs.writeFileSync("./map.json", JSON.stringify(fileInfo, null, "\t"))
     })
+
 }
+
